b60bb78b8ce10e61694587956b56117233176b87
lbarrettCIS497 <lbarrett@mail.bradley.edu>
2021-04-26 11:00:30 -0500

    Fixed runtime error caused by MealMaker.cs

--- Assets/Scripts/Meal Scripts/MealMaker.cs
+++ Assets/Scripts/Meal Scripts/MealMaker.cs
@@ -15,7 +15,6 @@ public class MealMaker : MonoBehaviour
     {
         gameManager = GameManager.instance;
         drinkSet = false;
-        DisplayMeal();
     }
 
     public void setMainCourse(string newMeal)
